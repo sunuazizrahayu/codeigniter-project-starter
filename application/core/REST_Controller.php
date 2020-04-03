367fa668c53ed31759dcf57f39321f3de5870341
Sunu <sunuazizrahayu@gmail.com>
2020-04-03 23:49:51 +0700

    add rest server

---
+++ application/core/REST_Controller.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+
+class REST_Controller extends RestController {
+
+	public function __construct()
+	{
+		parent::__construct();
+		//Do your magic here
+	}
+
+}
+
+/* End of file REST_Controller.php */
+/* Location: ./application/core/REST_Controller.php */
